1f0e00391ccab273192d1cd47424f2fa85c5a9a8
Nishant-Pall <palln6935@gmail.com>
2021-08-26 21:00:17 +0530

    Document namespaces, filters

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,32 +1,57 @@
 import * as esbuild from "esbuild-wasm";
 
+// returns a plugin object
+// esbuild is a little more streamlined than webpack
+// in terms of time and space efficiency
 export const unpkgPathPlugin = () => {
     return {
+        // name of plugin
         name: "unpkg-path-plugin",
+        // setup of plugin, called automatically called by esbuild as build
         setup(build: esbuild.PluginBuild) {
+            // overrides natural build process of trying to figure out
+            // the path of the file, it will prevent esbuild to look
+            // into the file system
+            // filter is an re, we can make different onResolve
+            // functions depending upon the type of files we want to load
+            // eg. for Javascript --> filter: '/.js/
+            // for TypeScript --> filter: '/.ts/
+            // for CSS --> filter: '/.css/
             build.onResolve({ filter: /.*/ }, async (args: any) => {
                 // console.log("onResolve", args);
+                // onLoad or any other functions will only
+                // work on files with namespace a
                 return { path: args.path, namespace: "a" };
             });
+            // attempts to load up a file
+            // also overrides the natural loading a file
+            // which is directly looking up the file system
+            build.onLoad(
+                { filter: /.*/, namespace: "a" },
+                async (args: any) => {
+                    // console.log("onLoad", args);
 
-            build.onLoad({ filter: /.*/ }, async (args: any) => {
-                // console.log("onLoad", args);
-
-                if (args.path === "index.js") {
-                    return {
-                        loader: "jsx",
-                        contents: `
+                    // custom object we return onLoad
+                    if (args.path === "index.js") {
+                        return {
+                            loader: "jsx",
+                            contents: `
               import message from './message';
               console.log(message);
             `,
-                    };
-                } else {
-                    return {
-                        loader: "jsx",
-                        contents: 'export default "hi there!"',
-                    };
+                        };
+                    } else {
+                        return {
+                            loader: "jsx",
+                            contents: 'export default "hi there!"',
+                        };
+                    }
                 }
-            });
+            );
         },
     };
 };
+
+// If esbuild finds any imports/require/exporst, esbuild calls
+// the onResolve, onLoad functions again
+// this process is carried out recursively
