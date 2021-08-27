4eccce7be9dcd3108d6696ddc0f6927fbe513252
Nishant-Pall <palln6935@gmail.com>
2021-08-28 02:39:39 +0530

    ResolveDir for getting exact importer path

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -35,7 +35,10 @@ export const unpkgPathPlugin = () => {
                 if (args.path.includes("./") || args.path.includes("../")) {
                     return {
                         namespace: "a",
-                        path: new URL(args.path, args.importer + "/").href,
+                        path: new URL(
+                            args.path,
+                            "https://unpkg.com" + args.resolveDir + "/"
+                        ).href,
                     };
                 }
                 return {
@@ -57,17 +60,20 @@ export const unpkgPathPlugin = () => {
                         return {
                             loader: "jsx",
                             contents: `
-              const message = require('medium-test-pkg') ;
+              const message = require('nested-test-pkg') ;
               console.log(message);
             `,
                         };
                     }
 
                     // axios get request to get the contents of the file
-                    const { data } = await axios.get(args.path);
+                    const { data, request } = await axios.get(args.path);
                     return {
                         loader: "jsx",
                         contents: data,
+                        // will return the path of the last importer to maintain
+                        // persistence of path from unpkg modules
+                        resolveDir: new URL("./", request.responseURL).pathname,
                     };
                 }
             );
