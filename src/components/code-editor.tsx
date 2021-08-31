d76b46e9d2edeb497215bcd5dda3319dd9ad5e1c
Nishant-Pall <palln6935@gmail.com>
2021-09-01 00:57:13 +0530

    Configure editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,6 +1,21 @@
 import MonacoEditor from "@monaco-editor/react";
 
 const CodeEdtior = () => {
-    return <MonacoEditor theme="dark" language="javascript" height="200px" />;
+    return (
+        <MonacoEditor
+            options={{
+                wordWrap: "on",
+                minimap: { enabled: false },
+                showUnused: false,
+                folding: false,
+                lineNumbersMinChars: 3,
+                fontSize: 18,
+                scrollBeyondLastLine: false,
+            }}
+            theme="dark"
+            language="javascript"
+            height="200px"
+        />
+    );
 };
 export default CodeEdtior;
