b22aca8be3a2edc7b714fb11898d74d39c3b4ae8
Nishant-Pall <palln6935@gmail.com>
2021-09-02 18:09:41 +0530

    Add listener for async errors

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -6,25 +6,40 @@ interface PreviewProps {
 }
 
 const html = `
-    <html>
-      <head>
-      <style>html{background-color:white;}</style>
-      </head>
-      <body>
-        <div id="root"></div>
-        <script>
-          window.addEventListener('message', (event) => {
-            try {
-              eval(event.data);
-            } catch (err) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-              console.error(err);
-            }
-          }, false);
-        </script>
-      </body>
-    </html>
+  <html>
+    <head>
+      <style>
+        html {
+          background-color: white;
+        }
+      </style>
+    </head>
+
+    <body>
+      <div id="root"></div>
+      <script>
+        const handleError = (err) => {
+          const root = document.querySelector('#root');
+          root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+          console.error(err);
+        };
+
+        window.addEventListener('error', (event)=>{
+          event.preventDefault();
+          handleError(event.error);
+        })
+
+        window.addEventListener('message', (event) => {
+          try {
+            eval(event.data);
+          } catch (err) {
+            handleError(err);
+          }
+        }, false);
+      </script>
+    </body>
+
+  </html>
   `;
 
 const Preview: React.FC<PreviewProps> = ({ code }) => {
